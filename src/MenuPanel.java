f91635d5dc4266bd563904b7af0f0918042268e0
yevgeny-sh <yevgst7@gmail.com>
2026-08-16 09:18:24 +0300

    Improve side menu layout

--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -8,9 +8,15 @@ public class MenuPanel extends JPanel {
     public MenuPanel(int x, int y, int width, int height) {
 
         this.setBounds(x, y, width, height);
-        this.setBackground(new Color(225, 225, 225));
+        this.setBackground(new Color(245, 245, 245));
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        this.add(Box.createVerticalStrut(50));
 
         this.scoreLabel = new JLabel("Score: 0");
+        this.scoreLabel.setFont(new Font("Arial", Font.BOLD, 26));
+        this.scoreLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         this.add(scoreLabel);
     }
